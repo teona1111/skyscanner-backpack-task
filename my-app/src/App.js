2c3c3b848ae4036d25515fec0f0aa3b6c4f41e34
teona1111 <teona.mehandjiska@student.um.si>
2026-08-15 12:24:57 +0000

    Add flight schedule calendar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,109 @@
-import React from 'react';
-import { BpkCode } from '@skyscanner/backpack-web/bpk-component-code';
+import React, { useState } from 'react';
+import { format } from 'date-fns';
+
 import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
 import BpkText from '@skyscanner/backpack-web/bpk-component-text';
-
+import BpkCalendar, {
+  CALENDAR_SELECTION_TYPE,
+} from '@skyscanner/backpack-web/bpk-component-calendar';
 import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
 
 import STYLES from './App.scss';
 
 const getClassName = cssModules(STYLES);
 
-const App = () => (
-  <div className={getClassName('App')}>
-    <header className={getClassName('App__header')}>
-      <div className={getClassName('App__header-inner')}>
-        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Welcome to React + Backpack</BpkText>
-      </div>
-    </header>
-    <main className={getClassName('App__main')}>
-      <BpkText tagName="p" className={getClassName('App__text')}>
-        To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
-      </BpkText>
-      <BpkButton onClick={() => alert('It works!')}>Click me</BpkButton>
-    </main>
-  </div>
-);
+const daysOfWeek = [
+  {
+    name: 'Sunday',
+    nameAbbr: 'Sun',
+    index: 0,
+    isWeekend: true,
+  },
+  {
+    name: 'Monday',
+    nameAbbr: 'Mon',
+    index: 1,
+    isWeekend: false,
+  },
+  {
+    name: 'Tuesday',
+    nameAbbr: 'Tue',
+    index: 2,
+    isWeekend: false,
+  },
+  {
+    name: 'Wednesday',
+    nameAbbr: 'Wed',
+    index: 3,
+    isWeekend: false,
+  },
+  {
+    name: 'Thursday',
+    nameAbbr: 'Thu',
+    index: 4,
+    isWeekend: false,
+  },
+  {
+    name: 'Friday',
+    nameAbbr: 'Fri',
+    index: 5,
+    isWeekend: false,
+  },
+  {
+    name: 'Saturday',
+    nameAbbr: 'Sat',
+    index: 6,
+    isWeekend: true,
+  },
+];
+
+const formatDateFull = date => format(date, 'EEEE, do MMMM yyyy');
+
+const formatMonth = date => format(date, 'MMMM yyyy');
+
+const App = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const selectionConfiguration = {
+  type: CALENDAR_SELECTION_TYPE.single,
+  date: selectedDate,
+  };
+  return (
+    <div className={getClassName('App')}>
+      <header className={getClassName('App__header')}>
+        <div className={getClassName('App__header-inner')}>
+          <BpkText
+            tagName="h1"
+            textStyle="xxl"
+            className={getClassName('App__heading')}
+          >
+            Flight Schedule
+          </BpkText>
+        </div>
+      </header>
+
+      <main className={getClassName('App__main')}>
+        <BpkCalendar
+          id="flight-schedule-calendar"
+          daysOfWeek={daysOfWeek}
+          weekStartsOn={1}
+          formatDateFull={formatDateFull}
+          formatMonth={formatMonth}
+          changeMonthLabel="Change month"
+          nextMonthLabel="Next month"
+          previousMonthLabel="Previous month"
+          month={currentMonth}
+          onMonthChange={(event, { month }) => setCurrentMonth(month)}
+          onDateSelect={setSelectedDate}
+          selectionConfiguration={selectionConfiguration}
+        />
+
+        <BpkButton onClick={() => alert('It works!')}>
+          Continue
+        </BpkButton>
+      </main>
+    </div>
+  );
+};
 
 export default App;
